63e0ca490e57884941ede37b691fdcdf3a92b83c
Oleg Mentuz <olegmentuz@gmail.com>
2020-07-06 08:35:16 +0300

    task2

---
+++ src/com/epam/task2/Main.java
@@ -0,0 +1,8 @@
+package com.epam.task2;
+
+public class Main {
+    public static void main(String[] args) {
+        for( int i = args.length - 1 ; i >= 0 ; i--)
+            System.out.println("Обратный порядок           : " + args[i]);
+    }
+}
